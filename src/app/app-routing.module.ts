6f04d9854e0f430dce327e8ee7d5fb0d59dfd666
Jatinder kumars <jatinderkumar.softwaredeveloper@gmail.com>
2026-05-11 08:48:40 +0530

    fixed routing and logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -301,7 +301,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top', useHash: true })],
   exports: [RouterModule]
 })
  
